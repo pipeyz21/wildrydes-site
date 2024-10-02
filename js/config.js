dad890ce1e5ec64c0bc61f52c9b21a47cadeee19
Felipe YZ <felipe.yan13@gmail.com>
2024-10-01 23:04:46 -0300

    new config3

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_XJHonTV3c', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '01ab1550-d0b1-70d3-773d-d61d022dc331', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '7dpt4f3cn51bc25kdam3g15alc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
